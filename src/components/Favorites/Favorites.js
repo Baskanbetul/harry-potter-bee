36bb8675ec746c03290e05d2c2a304b11e0c9e93
Baskanbetul <btlbskn@hotmail.com>
2022-08-07 18:45:14 -0600

    Add favorites styling

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -3,28 +3,32 @@ import './Favorites.css';
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 
-const Favorites = ({ favoriteCharacters, removeFavorite }) => { //, id 
+const Favorites = ({ favoriteCharacters, removeFavorite }) => {
   const formatCharacters = favoriteCharacters.map(favoriteCharacter => {
     return (
-      <div key={favoriteCharacter.id}>
-      <img className='character-poster' src={favoriteCharacter.image} alt={`${favoriteCharacter.name} information`}/>
-      <p>Name: {favoriteCharacter.character}</p>
-      <p>Nickname: {favoriteCharacter.nickname}</p>
-      <p>Is {favoriteCharacter.character} a hogwarts student?:{favoriteCharacter.hogwartsStudent ? 'YES' : 'NO'}</p>
-      <p>{favoriteCharacter.character}' house:{favoriteCharacter.hogwartsHouse}</p>
-      <p>Interpreted By:{favoriteCharacter.interpretedBy}</p>
-      <p>{favoriteCharacter.child[0]} {favoriteCharacter.child[1]} {favoriteCharacter.child[2]}</p>
-      <button className="trash-btn" onClick={() => removeFavorite(favoriteCharacter.id)}>🗑</button>
+      <div className='favorite-container' key={favoriteCharacter.id}>
+        <img className='character-poster' src={favoriteCharacter.image} alt={`${favoriteCharacter.name} information`}/>
+        <div className='favorite-info'>
+          <p>{favoriteCharacter.character}</p>
+          <p>Nickname: {favoriteCharacter.nickname}</p>
+          <p>Is {favoriteCharacter.character} is {favoriteCharacter.hogwartsStudent ? 'a hogwarts student.' : 'not a hogwarts student.'}.</p>
+          <p>{favoriteCharacter.character}'s house is {favoriteCharacter.hogwartsHouse}.</p>
+          <p>This character is interpreted by {favoriteCharacter.interpretedBy}.</p>
+          <p>{favoriteCharacter.child ? favoriteCharacter.child.join(' and ') : ' '}</p> 
+          <button className="trash-btn" onClick={() => removeFavorite(favoriteCharacter.id)}>🗑</button>
+        </div>
      </div>
     )
   })
   return (
-   <div className="favorites">
+    <>
+   {/* <div className="favorites"> */}
     {formatCharacters}
     <Link to ='/'>
-    <button>GO BACK</button>
+    <button className='go-back-bttn'>GO BACK</button>
     </Link>
-    </div>
+    {/* </div> */}
+    </>
   )
 }
 
